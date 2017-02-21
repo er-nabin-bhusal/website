0090d2378693227aa815f3c749dc8c9cf8de626a
nabinbhusal80 <nabinbhusal80@gmail.com>
2017-02-21 19:11:23 +0545

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from kanchanpur import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -28,6 +26,3 @@ urlpatterns = [
     url(r'^ticket$',views.ticket_view,name='ticket'),
 
 ]
-if settings.DEBUG:
-    urlpatterns +=static (settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
