865ab07f2531766b11bb55df922f0818931d1f5b
nabinbhusal80 <nabinbhusal80@gmail.com>
2017-02-21 19:25:03 +0545

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2tmj@m40xgg^f9epfo%$h45im+sv&#uuu42nrftu)v(2hxc(6p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','192.168.100.5']
+ALLOWED_HOSTS = ['104.236.239.144']
 
 
 # Application definition
